baef8097fff07ba6d62053554d58a51b5aef88d6
Fedor Pasynkov <76725612+pefbrute@users.noreply.github.com>
2023-11-23 06:10:15 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,59 @@
+import pyperclip
+import pyautogui
+import time
+
+def copy_to_clipboard():
+    """Copies the selected text to clipboard and returns it."""
+    pyautogui.hotkey('ctrl', 'c')  # 'command' on macOS
+    time.sleep(0.1)  # Let the clipboard catch up
+    return pyperclip.paste()
+
+def cut_first_line(input_text):
+    """Cuts the first line from the given text and copies it to clipboard."""
+    lines = input_text.split("\n")
+    pyperclip.copy(lines.pop(0))
+    return "\n".join(lines)
+
+def paste_from_clipboard():
+    """Pastes text from the clipboard."""
+    pyautogui.hotkey('ctrl', 'v')  # 'command' on macOS
+
+def navigate_fields():
+    """Navigates to the next field."""
+    pyautogui.hotkey('tab')
+
+def submit_form():
+    """Submits the current form."""
+    pyautogui.hotkey('ctrl', 'enter')  # 'command' on macOS
+
+def fill_anki_card():
+    """Processes the text block and manages the flashcard formatting."""
+    pyautogui.hotkey('ctrl', 'a')  # 'command' on macOS
+    input_text = copy_to_clipboard()
+
+    if input_text:
+        # Take the first line and paste it
+        remaining_text = cut_first_line(input_text)
+        paste_from_clipboard()
+        navigate_fields()
+        navigate_fields()
+
+        # Take the next line and paste it
+        remaining_text = cut_first_line(remaining_text)
+        paste_from_clipboard()
+        submit_form()
+
+        # Update the clipboard with the remaining text
+        pyperclip.copy(remaining_text)
+        paste_from_clipboard()
+        
+        return True
+    return False
+
+def convert_flashcard_format():
+    """Keeps processing text blocks until there's no text left."""
+    while fill_anki_card():
+        # The loop will call process_text_block() until it returns False
+        time.sleep(0.1)  # Prevent potential for rapid-fire input
+
+convert_flashcard_format()
